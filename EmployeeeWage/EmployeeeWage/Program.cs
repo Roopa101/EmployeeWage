52f7239129027f391e7b76b29e968e8af63f37b3
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-04 17:01:35 +0530

    [Roopa] Checking whether Employee is present or absent

--- EmployeeeWage/EmployeeeWage/Program.cs
+++ EmployeeeWage/EmployeeeWage/Program.cs
@@ -11,7 +11,21 @@ namespace EmployeeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Problem");
-            Console.ReadLine();
-        }
+            int isPresent = 1;
+
+            Random employee = new Random();
+            int randomInput = employee.Next(0, 2);
+
+            if (randomInput == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+                Console.ReadLine();
+        } 
     }
 }
+      
