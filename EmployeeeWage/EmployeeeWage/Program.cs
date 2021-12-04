2585fee93553692bc5f5688cd78cb39c0aa42aa9
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-04 17:05:01 +0530

    [Roopa] Checking Daily Employee wage

--- EmployeeeWage/EmployeeeWage/Program.cs
+++ EmployeeeWage/EmployeeeWage/Program.cs
@@ -12,20 +12,27 @@ namespace EmployeeeWage
         {
             Console.WriteLine("Welcome to EmployeeWage Problem");
             int isPresent = 1;
+            int empWage = 0;
+            int ratePerHrs = 20;
+            int empHrs = 0;
 
             Random employee = new Random();
             int randomInput = employee.Next(0, 2);
 
             if (randomInput == isPresent)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
-                Console.ReadLine();
-        } 
+            empWage = ratePerHrs * empHrs;
+            Console.WriteLine("Employee wage per day :" + empWage);
+            Console.ReadLine();
+
+
+        }
     }
 }
-      
