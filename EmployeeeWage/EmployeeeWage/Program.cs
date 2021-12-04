fdb82a8734eca0b4b6b28192734add2fe705b84f
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-04 17:09:27 +0530

    [Roopa] Checking Employee is present or absent and wage per day

--- EmployeeeWage/EmployeeeWage/Program.cs
+++ EmployeeeWage/EmployeeeWage/Program.cs
@@ -11,28 +11,32 @@ namespace EmployeeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Problem");
-            int isPresent = 1;
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int empWage = 0;
             int ratePerHrs = 20;
             int empHrs = 0;
 
             Random employee = new Random();
-            int randomInput = employee.Next(0, 2);
+            int randomInput = employee.Next(0, 3);
 
-            if (randomInput == isPresent)
+            if (randomInput == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("FullTimeEmp is Present");
                 empHrs = 8;
+            }
+            else if (randomInput == IS_PART_TIME)
+            {
+                Console.WriteLine("PartTimeEmp is Present");
+                empHrs = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
             empWage = ratePerHrs * empHrs;
-            Console.WriteLine("Employee wage per day :" + empWage);
+            Console.WriteLine("Emp Wage per day:" + empWage);
             Console.ReadLine();
-
-
         }
     }
 }
