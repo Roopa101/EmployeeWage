63b1397d250fc44b3c39a24e8f19faa0b03c5cf1
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-04 16:55:05 +0530

    [Roopa]Displaying welcome to Employee wage program

---
+++ EmployeeeWage/EmployeeeWage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage Problem");
+            Console.ReadLine();
+        }
+    }
+}
