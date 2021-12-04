5b7fbe3cb184f100530f9adb4fd369c85fbc2de0
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-04 17:18:44 +0530

    [Roopa] Checking Employee Wage For A Month

--- EmployeeeWage/EmployeeeWage/Program.cs
+++ EmployeeeWage/EmployeeeWage/Program.cs
@@ -11,31 +11,38 @@ namespace EmployeeeWage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int ratePerHrs = 20;
+        public const int NO_OF_WORKING_DAYS = 20;
         public static void Main(string[] args)
         {
 
             int empWage = 0;
             int empHrs = 0;
+            int totalEmpWage = 0;
             string typeOfEmp = "";
 
-            Random employee = new Random();
-            int randomInput = employee.Next(0, 3);
-
-            switch (randomInput)
+            for (int day = 0; day <= NO_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
+                Random random = new Random();
+                int randomInput = random.Next(0, 3);
 
-            empWage = ratePerHrs * empHrs;
-            Console.WriteLine(typeOfEmp + "  Wage per day is :" + empWage);
+                switch (randomInput)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWage = ratePerHrs * empHrs;
+                Console.WriteLine("{0} Emp of the day {1} Wage is {2} ", typeOfEmp, day, empWage);
+                totalEmpWage = totalEmpWage + empWage;
+            }
+            Console.WriteLine("Employee Wage for {0} day is :{1}", NO_OF_WORKING_DAYS, totalEmpWage);
             Console.ReadLine();
         }
     }
